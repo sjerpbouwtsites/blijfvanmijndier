eacdac8461180f9dbaf8d8525376437c2752596f
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-10-07 21:43:24 +0200

    map init; dummy data

--- resources/assets/js/map.js
+++ resources/assets/js/map.js
@@ -1,3 +1,6 @@
+/**
+ * intialises map on roelofarendsveen and returns leaflet map instance.
+ */
 function createMap() {
   const goudaMapConfig = {
     lat: 52.2,
@@ -26,4 +29,108 @@ function createMap() {
   return leafletMap;
 }
 
-createMap();
+/**
+ * dummy function to get locations
+ * @returns Promise always succes with dummydata
+ */
+function getLocations() {
+  return new Promise((locationSucces, locationFailure) => {
+    setTimeout(() => {
+      return locationSucces(dummyData);
+    }, 250);
+  });
+}
+
+/**
+ * create alt attribute which is a general styling & identifying attribute in this app for markers.
+ * loops over list of conditions
+ * @param {*} location
+ */
+function maakAlt(location) {
+  return [
+    {
+      key: "type",
+      check: (str) => {
+        return str === "vet";
+      },
+      res: "color-red",
+    },
+  ]
+    .map((condition) => {
+      const locationVal = location[condition.key];
+
+      return condition.check(locationVal) ? condition.res : "";
+    })
+    .join(" ");
+}
+
+/**
+ * callback for locations.map
+ * refers maakAlt
+ * creates marker and binds to global mapInstance
+ * bindspopup.
+ * @param {*} location
+ */
+function locationMapper(location) {
+  const options = {
+    alt: maakAlt(location),
+  };
+
+  const marker = L.marker([location.lat, location.lon], options).addTo(leafletMap);
+
+  marker.bindPopup(
+    `<div class='leaflet-popup-publicatie'>
+      <header class='leaflet-popup-header'>
+        
+        <span class='leaflet-popup-header-left'>
+          ${location.title}
+        </span>
+        
+        <div class='leaflet-popup-header-right'>
+          
+        </div>
+          
+      </header>
+
+      <div class='leaflet-popup-text verborgen'>
+        ${location.text}
+      </div>
+
+      </div class='leaflet-popup-publicatie'>`
+  );
+}
+
+const dummyData = [
+  {
+    type: "vet",
+    lat: 52.2,
+    lon: 4.6,
+    title: "harry",
+    text: "harry text",
+  },
+  {
+    type: "vet",
+    lat: 52.25,
+    lon: 4.65,
+    title: "harry 2",
+    text: "harry textdf dgfdg",
+  },
+  {
+    type: "bakery",
+    lat: 53.25,
+    lon: 4.65,
+    title: "bakery",
+    text: "bread is good",
+  },
+];
+
+let leafletMap;
+
+function init() {
+  leafletMap = createMap();
+  getLocations().then((locations) => {
+    locations.map(locationMapper);
+  });
+}
+
+init();
