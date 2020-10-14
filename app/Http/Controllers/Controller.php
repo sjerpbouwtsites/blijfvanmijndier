0ce0cee8c1f6862015a7267d72c0789f855a3c65
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-10-14 09:21:07 +0200

    added map to menu items

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,7 +19,7 @@ class Controller extends BaseController
 
     public $breedId;
     public $behaviourId;
-    public $vaccinationId;    
+    public $vaccinationId;
     public $animaltypeId;
     public $hometypeId;
     public $gendertypeId;
@@ -29,10 +29,11 @@ class Controller extends BaseController
     public $updatetypeId;
     public $updatetypeOwner;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->breedId = 1;
         $this->behaviourId = 2;
-        $this->vaccinationId = 3;    
+        $this->vaccinationId = 3;
         $this->animaltypeId = 4;
         $this->hometypeId = 5;
         $this->gendertypeId = 6;
@@ -43,78 +44,88 @@ class Controller extends BaseController
         $this->updatetypeOwner = 179;
     }
 
-    public function getUpdateDate(){
+    public function getUpdateDate()
+    {
         $date = new DateTime();
         $date->modify('-14 day');
         return $date->format('Y-m-d H:i:s');
     }
 
-    public function animalNeedUpdate($id){
+    public function animalNeedUpdate($id)
+    {
         $animal = Animal::find($id);
 
-        if(!$animal->updates){
+        if (!$animal->updates) {
             return false;
         }
 
-        $update = Update::where([['updatetype_id', $this->updatetypeOwner],['link_type', 'animals'],['link_id', $id]])->orderBy('start_date', 'desc')->first();    
+        $update = Update::where([['updatetype_id', $this->updatetypeOwner], ['link_type', 'animals'], ['link_id', $id]])->orderBy('start_date', 'desc')->first();
 
-        if($update == null){
+        if ($update == null) {
             return ($animal->registration_date < $this->getUpdateDate());
-        } 
+        }
 
-        if($update->start_date < $this->getUpdateDate()){
+        if ($update->start_date < $this->getUpdateDate()) {
             return true;
         }
 
         return false;
     }
 
-    public function getAnimalImage($id){
+    public function getAnimalImage($id)
+    {
         $image = 'img/' . 'animal_' . $id . '.jpg';
 
-        if(file_exists($image)){
+        if (file_exists($image)) {
             return $image;
         }
 
         return 'img/placeholder.jpg';
     }
 
-    public function GetMenuItems($currentPath){
-        $menuItem[] = $this->GetMenuItem('Dieren', 'animals', '', 'fa-paw', $currentPath); 
-        $menuItem[] = $this->GetMenuItem('Eigenaren', 'owners', '', 'fa-female', $currentPath); 
-        $menuItem[] = $this->GetMenuItem('Dierenartsen', 'vets', '', 'fa-user-md', $currentPath); 
-        $menuItem[] = $this->GetMenuItem('Opvanglocaties', 'locations', '', 'fa-building', $currentPath); 
-        $menuItem[] = $this->GetMenuItem('Gastgezinnen', 'guests', '', 'fa-users', $currentPath); 
-        $menuItem[] = $this->GetMenuItem('Pensions', 'shelters', '', 'fa-home', $currentPath); 
-        $menuItem[] = $this->GetMenuItem('Tabellen', 'tables', '', 'fa-cog', $currentPath); 
+    public function GetMenuItems($currentPath)
+    {
+        $menuItem[] = $this->GetMenuItem('Dieren', 'animals', '', 'fa-paw', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Eigenaren', 'owners', '', 'fa-female', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Dierenartsen', 'vets', '', 'fa-user-md', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Opvanglocaties', 'locations', '', 'fa-building', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Gastgezinnen', 'guests', '', 'fa-users', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Pensions', 'shelters', '', 'fa-home', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Tabellen', 'tables', '', 'fa-cog', $currentPath);
+        $menuItem[] = $this->GetMenuItem('Kaart', 'map', '', 'fa-map', $currentPath);
 
         return $menuItem;
     }
 
-    public function GetMenuItem($title, $path, $classes, $icon, $currentPath){
+    public function GetMenuItem($title, $path, $classes, $icon, $currentPath)
+    {
         $active  = $path == $currentPath ? 'active' : '';
         $classes = $classes != '' ? $classes + ' ' + $active : $active;
 
-        return new MenuItem($title, $path, $classes, $icon); 
+        return new MenuItem($title, $path, $classes, $icon);
     }
 
-    public function FormatDate($date){
+    public function FormatDate($date)
+    {
         return date("d-m-Y", strtotime($date));
     }
 
-    public static function FormatDateTime($date){
+    public static function FormatDateTime($date)
+    {
         return date("d-m-Y H:i:s", strtotime($date));
     }
 
-    public function getDescription($id){
+    public function getDescription($id)
+    {
         $table = Table::find($id);
-        if($table){
+        if ($table) {
             return $table->description;
         }
         return "Onbekend";
     }
 
-    public function GetTableList($tablegroupId){
+    public function GetTableList($tablegroupId)
+    {
         $tableList = Table::All()->where('tablegroup_id', $tablegroupId)->sortBy('description');
         $tableList = $tableList->pluck('description', 'id');
 
