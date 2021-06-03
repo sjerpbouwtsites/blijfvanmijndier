eafaf5a5bb4017f4f2ee7b9de3b5daa150d3a0a0
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-06-04 00:09:15 +0200

    bagger

--- database/lokaal-backup.sql
+++ database/lokaal-backup.sql
@@ -32,7 +32,7 @@ CREATE TABLE `addresses` (
   `lattitude` text,
   `longitude` text,
   PRIMARY KEY (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
