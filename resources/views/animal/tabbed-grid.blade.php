f7818509e6f74674a1e1064b80480cd60ab99bfa
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-06-24 14:08:17 +0200

    achterstand eringezet

--- resources/views/animal/tabbed-grid.blade.php
+++ resources/views/animal/tabbed-grid.blade.php
@@ -5,7 +5,6 @@
         <div class='animal-grid__image-outer'>
             <img loading='lazy' class="animal-grid__image" src="{{ $animal->animalImage }}" alt="{{ $animal->name }}" width="180" height="180">
             
-            
             @if ($animal->updates_checked['has_icons'])
             <ul class='animal-grid__icons'>
                 @foreach ($animal->updates_checked['icons'] as $icon)
@@ -24,16 +23,14 @@
         <div class='animal-grid__block-footer'>
             
             @if ($animal->updates_checked['has_prompts'])
-                <ol class='animal-grid__prompts'>
+
+                <ol class='animal-grid__prompts' title='weken achter op planning: {{\round($animal->updates_checked['days_behind'] / 7)}}'>
                 @foreach ($animal->updates_checked['update_prompts'] as $prompt)
                     <li class='animal-grid__prompt-item'>{{$prompt}}</li>
                 @endforeach
                 </ol>
             @endif
 
-            {{-- @if ($animal->updates_checked['in_todo_list']) 
-                <span class='animal-grid__days-to-late'>dagen te laat:{{$animal->updates_checked['days_behind']}}</span>
-            @endif --}}
         </div>
         
     </a>
