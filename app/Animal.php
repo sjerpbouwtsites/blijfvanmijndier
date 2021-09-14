f9413ca21d4e8cc84a4043a348dc62aba686a862
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-09-14 10:55:00 +0200

    bug in OR

--- app/Animal.php
+++ app/Animal.php
@@ -95,10 +95,11 @@ class Animal extends Model
 		 * As this would sygnify that the pet is with the owner
 		 */
 		$animal_updates_checkbox_bool = $animal['attributes']['updates'];
-		$animal_is_with_owner = !\is_null($animal['attributes']['owner_id']) || !\is_null($animal['attributes']['guest_id']);
-
+		$animal_is_with_owner = (!\is_null($animal['attributes']['owner_id'])) && \is_null($animal['attributes']['guest_id']);
 		$needs_owner_updates = $animal_updates_checkbox_bool && !$animal_is_with_owner;
 
+
+	
 		// gastgezin
 		// hulpverlening
 		// pension
@@ -155,6 +156,8 @@ class Animal extends Model
 			$uc['needs_jaarevaluatie_update'] = true;
 		}
 
+		// THIS IS AN ISSUE WHEN THE APP GROWS! FOR EVERY ANIMAL NEW QUERY
+		// BETTER: JOIN OF SORTS? BUT HOW
 		$updates = DB::select("SELECT * FROM updates WHERE link_type = 'animals' AND link_id = $animal_id");
 
 		if (count($updates) > 0) {
