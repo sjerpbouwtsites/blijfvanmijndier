7e4d9c2972101caef55a67f8431234076e7965a5
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-09-15 06:17:42 +0200

    begin kopy adres; verwijderd legacy index

---
+++ resources/views/generic/address-copy.blade.php
@@ -0,0 +1,5 @@
+<div class='kopieer-adres' data-street='' data-postal_code='' data-city='' data-house_number=''>
+    <button class="kopieer-adres-knop" title="Kopieer adres" data-href="0616541143">
+        <i class="fa fa-save"></i>
+    </button>
+</div>
